e1b5ac76e5e2bafdc69f58e9a0d40b71234ae70f
Max Schmitt <max@schmitt.mx>
2024-03-13 15:42:09 +0100

    nit

--- tests/examples.spec.ts
+++ tests/examples.spec.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import { test, expect } from "@playwright/test"
-import cp from 'child_process';
+import cp, { spawn } from 'child_process';
 
 const kExamplesRootDirectory = path.join(__dirname, '../examples')
 const examples = fs.readdirSync(kExamplesRootDirectory);
@@ -9,11 +9,16 @@ const examples = fs.readdirSync(kExamplesRootDirectory);
 for (const example of examples) {
   test(`example ${example} should work`, async ({  }) => {
     const exampleDirectory = path.join(kExamplesRootDirectory, example);
-    const result = cp.spawnSync('npm', ['run', 'test'], {
-      cwd: exampleDirectory,
-      stdio: 'inherit',
-      shell: true,
-    });
-    expect(result.status).toBe(0);
+    spawnAndAssert('npm', ['ci'], exampleDirectory);
+    spawnAndAssert('npm', ['run', 'test'], exampleDirectory);
   });
+}
+
+function spawnAndAssert(command: string, args: string[], cwd: string) {
+  const result = cp.spawnSync(command, args, {
+    cwd,
+    stdio: 'inherit',
+    shell: true,
+  });
+  expect(result.status).toBe(0);
 }
