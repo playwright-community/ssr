e15dbe1f178f8c0711633e312ae6795534eeb193
Max Schmitt <max@schmitt.mx>
2024-03-17 11:03:43 +0100

    chore: move Next.js no-cache into fetchCache property

--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -1,5 +1,7 @@
+export const fetchCache = 'default-no-store'
+
 export default async function FruitsPage() {
-  const response = await fetch('https://demo.playwright.dev/api-mocking/api/v1/fruits?1', { cache: 'no-cache' });
+  const response = await fetch('https://demo.playwright.dev/api-mocking/api/v1/fruits?1');
   const fruits = await response.json() as any[];
   console.log('doing the server side request')
 
