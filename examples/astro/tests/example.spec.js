efd663b4eb4ed86a6ea982a2ef0b7beec8c206fa
Max Schmitt <max@schmitt.mx>
2024-03-17 10:58:54 +0100

    test: add test for Astro

---
+++ examples/astro/tests/example.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from 'playwright-ssr';
+
+test('my test', async ({ page, webServer }) => {
+  await webServer.route('**/*', async route => {
+    await route.fulfill({
+      status: 200,
+      json: [{ id: 1, name: 'John' }, { id: 2, name: 'Doe' }],
+    })
+  });
+  await page.goto("http://localhost:4321")
+  await expect(page.getByRole('listitem')).toHaveText([
+    'John',
+    'Doe',
+  ])
+});
