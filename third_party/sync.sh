b20239a33c4a9f615da4e80e1c462907a0364583
Max Schmitt <max@schmitt.mx>
2024-03-21 15:32:46 +0100

    test: add nuxt example (#12)

--- third_party/sync.sh
+++ third_party/sync.sh
@@ -7,11 +7,37 @@ trap "cd $(pwd -P)" EXIT
 cd "$(dirname "$0")"
 SCRIPT_FOLDER="$(pwd -P)"
 
-rm -rf interceptors
-git clone https://github.com/playwright-community/msw-interceptors interceptors
-cd interceptors
-git checkout RemoteHttpInterceptor-fixes
-npm i -g pnpm
+USAGE=$(cat<<EOF
+  usage: $(basename "$0") [--rebuild]
+
+  --rebuild: Rebuild the interceptors package without syncing the source code.
+EOF
+)
+
+if [[ $1 == "--help" || $1 == "-h" ]]; then
+  echo "$USAGE"
+  exit 0
+fi
+
+args=("$@")
+IS_REBUILD=""
+for ((i="${#args[@]}"-1; i >= 0; --i)); do
+    case ${args[i]} in
+        --rebuild) IS_REBUILD="1"; unset args[i]; ;;
+    esac
+done
+
+if [[ $IS_REBUILD == "" ]]; then
+    echo "Syncing the interceptors package..."
+    rm -rf interceptors
+    git clone https://github.com/playwright-community/msw-interceptors interceptors
+    cd interceptors
+    git checkout RemoteHttpInterceptor-fixes
+    npm i -g pnpm
+else
+    echo "Rebuilding the interceptors package..."
+    cd interceptors
+fi
 pnpm install
 OUT_DIR="$SCRIPT_FOLDER/../packages/playwright-ssr/third_party"
 npx esbuild src/RemoteHttpInterceptorWS.ts  --platform=node --bundle --outfile=$OUT_DIR/interceptors.js --sourcemap --format=cjs
